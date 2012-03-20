faf77fc3c83af9cbdc89558bb4ad90541b863a61
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-20 21:49:07 +0000

    A few minor updates.

---
+++ deploy/config/app.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file contains your applicaiton specific configuration.
+ * be aware that rocket.conf.php also exists and is overridden
+ * by this file, future upgrades for RockPack will not affect
+ * this file, if new configuration options are avaliable you will
+ * be able to copy and paste them from the rocket.conf.php into
+ * here if you need to override them. 
+ */
+
+global $app_config;
+
+$app_config['app_name'] = ""; // The name of the applicaiton.
+$app_config['system_path'] = ""; // The system path to the root of this application, including the trailing slash.
+$app_config['web_path'] = ""; // The web path to the root of this applicaiton, including the trailing slash.
+?>
---
+++ deploy/config/rocket.conf.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file contains the default framework configuration settings.
+ * these settings should be overridden in app.conf.php!
+ */
+
+global $app_config;
+
+$app_config = array(
+    "app_name" => "_new_rocket_app", // The name of the applicaiton.
+    "app_version" => "null", // The application version number
+    "system_path" => "/", // The system path to the root of this application, including the trailing slash.
+    "web_path" => "/", // The web path to the root of this applicaiton, including the trailing slash.
+);
+?>
