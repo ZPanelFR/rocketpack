abe0403edce127163fc3b777de8f78aa93a5f753
Bobby Allen <bobbyallen.uk@gmail.com>
2012-06-19 14:12:07 +0100

    Clean up some code.

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
 /**
  * RocketPack Configuration File
  */
-
 // Redirect on a non-specific controller request
 define('app_defaultcontroller', 'index');
 
@@ -16,8 +15,7 @@ define('app_db_pass', 'PASSWORD');
 
 define('app_salt', 'put your unique phrase/key here');
 define('app_urlrewrite', true);
-define('app_classpath', 'model/,lib/core/,lib/vendor/'); // Comma seperated class folders.
-
+define('app_classpath', 'model/,lib/core/,lib/vendor/'); // Comma seperated class folders, the classes in these folder will be automatically loaded.
 // Set the application absolute path..
 if (!defined('app_path'))
     define('app_path', dirname(__FILE__) . '/');
