b58c868dc9f7ecd30f66ffe21e43a0fc3f3d2bd3
Bobby Allen <bobbyallen.uk@gmail.com>
2012-06-26 11:09:46 +0100

    Quick clean up of some test code.

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 require 'config.php';
 require 'lib/core/bootstrap.php';
+
 $session = new sessionmanager();
 session_start();
+
 require 'lib/core/dispatcher.php';
-$_SESSION['example'] = "Hello this is my example session stuff right here!";
 ?>
