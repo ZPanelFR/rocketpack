a8c10048726c31b170fca45f064159b166fddb0d
Bobby Allen <bobbyallen.uk@gmail.com>
2012-06-26 16:49:42 +0100

    Added initial work to a 'pre checklist' of which will be displayed when the user first opens up RocketPack.

--- controller/launch.php
+++ controller/launch.php
@@ -2,7 +2,42 @@
 
 class launch extends rocketpack {
     
+    public function outPathLogs(){
+        return app_logpath;
+    }
     
+    public function outPathTemp(){
+        return app_tmppath;
+    }
+    
+    public function outPathCache(){
+        return app_cachepath;
+    }
+
+
+    public function outIsWritableLogs(){
+        return is_writable(app_logpath);
+    }
+    
+    public function outIsWritableCache(){
+        return is_writable(app_cachepath);
+    }
+    
+    public function outIsWritableTemp(){
+        return is_writable(app_tmppath);
+    }
+    
+    public function outIsExistLogs(){
+        return file_exists(app_logpath);
+    }
+    
+    public function outIsExistCache(){
+        return file_exists(app_cachepath);
+    }
+    
+    public function outIsExistTemp(){
+        return file_exists(app_tmppath);
+    }
     
 }
 
