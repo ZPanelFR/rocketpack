e528c5eb39021033620fd2fed8e7aec2f63cb59b
Bobby Allen <bobbyallen.uk@gmail.com>
2012-06-29 14:53:33 +0100

    Changed the authentication class to make it more user specific.

--- lib/core/user.class.php
+++ lib/core/user.class.php
@@ -1,12 +1,12 @@
 <?php
 
-class authentication extends rocketpack {
+class user extends rocketpack {
 
     /**
      * Checks to see if the user is logged in by checking if the 'rpuid' session is set.
      * @return boolean 
      */
-    public static function UserLoggedIn() {
+    public static function GetIsLoggedIn() {
         if (isset($_SESSION['rpuid']) && $_SESSION['rpuid'] != '')
             return true;
         return false;
@@ -17,8 +17,8 @@ class authentication extends rocketpack {
      * @param string $redirectcontroller The name of the 'login' or 'login required' controller to redirect the user too on a failed login attempt.
      * @return boolean 
      */
-    public static function RequireUser($redirectcontroller) {
-        if (self::UserLoggedIn())
+    public static function SetIsRequired($redirectcontroller) {
+        if (self::IsLoggedIn())
             return true;
         notice::ResetNotice();
         notice::StoreNotice("You are required to login to access that area, please login to continue..");
@@ -31,7 +31,7 @@ class authentication extends rocketpack {
      * @param int $uid The user ID to set the logged in user as.
      * @return boolean 
      */
-    public static function RegisterUserSesion($uid) {
+    public static function RegisterSesion($uid) {
         $_SESSION['rpuid'] = $uid;
         return true;
     }
@@ -40,7 +40,7 @@ class authentication extends rocketpack {
      * A static function to 'destory' the user's session.
      * @return boolean 
      */
-    public static function DestroyUserSesion() {
+    public static function DestroySesion() {
         $_SESSION['rpuid'] = null;
         return true;
     }
@@ -49,7 +49,7 @@ class authentication extends rocketpack {
      * A static function to return the details of the logged in user.
      * @return array Table data from the t_user table for the current logged-in user. 
      */
-    public static function GetLoggedInUserDetails() {
+    public static function GetDetails() {
         $user = $this->Database()->select("t_user", "us_id_pk = " . self::GetLoggedInUserID() . "");
         return $user;
     }
@@ -58,7 +58,7 @@ class authentication extends rocketpack {
      * Returns the current UID of the logged-in user.
      * @return int 
      */
-    public static function GetLoggedInUserID() {
+    public static function GetID() {
         return $_SESSION['rpuid'];
     }
 
