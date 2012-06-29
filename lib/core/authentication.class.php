71e0648c1a37141c9cf01fcc25a2c04903674f6c
Bobby Allen <bobbyallen.uk@gmail.com>
2012-06-29 12:25:48 +0100

    Added a generic user authentication method and basic t_user table which can be used if the user wishes. - Works with the default authentication class.

--- lib/core/authentication.class.php
+++ lib/core/authentication.class.php
@@ -3,41 +3,63 @@
 class authentication extends rocketpack {
 
     /**
-     * Checks to see if the current session is a logged in user.
+     * Checks to see if the user is logged in by checking if the 'rpuid' session is set.
      * @return boolean 
      */
     public static function UserLoggedIn() {
-        if (isset($_SESSION['zhm_uid']) && $_SESSION['zhm_uid'] != '')
+        if (isset($_SESSION['rpuid']) && $_SESSION['rpuid'] != '')
             return true;
         return false;
     }
 
-    public static function RequireUser() {
+    /**
+     * This static function should be used to 'require' access to a page, if the user is not logged in they will be redirected to the specified controller with a 'notice' message set.
+     * @param string $redirectcontroller The name of the 'login' or 'login required' controller to redirect the user too on a failed login attempt.
+     * @return boolean 
+     */
+    public static function RequireUser($redirectcontroller) {
         if (self::UserLoggedIn())
             return true;
         notice::ResetNotice();
         notice::StoreNotice("You are required to login to access that area, please login to continue..");
-        header("location: " .link::build('login'). "");
+        header("location: " . link::build($redirectcontroller) . "");
         exit;
     }
 
+    /**
+     * A static function to 'register' the user's session.
+     * @param int $uid The user ID to set the logged in user as.
+     * @return boolean 
+     */
     public static function RegisterUserSesion($uid) {
-        $_SESSION['zhm_uid'] = $uid;
-        return;
+        $_SESSION['rpuid'] = $uid;
+        return true;
     }
 
+    /**
+     * A static function to 'destory' the user's session.
+     * @return boolean 
+     */
     public static function DestroyUserSesion() {
-        $_SESSION['zhm_uid'] = null;
-        return;
+        $_SESSION['rpuid'] = null;
+        return true;
     }
 
+    /**
+     * A static function to return the details of the logged in user.
+     * @return array Table data from the t_user table for the current logged-in user. 
+     */
     public static function GetLoggedInUserDetails() {
-        $user = $this->Database()->select("t_user", "in_us_fk = " . self::GetLoggedInUserID() . "");
+        $user = $this->Database()->select("t_user", "us_id_pk = " . self::GetLoggedInUserID() . "");
         return $user;
     }
 
+    /**
+     * Returns the current UID of the logged-in user.
+     * @return int 
+     */
     public static function GetLoggedInUserID() {
-        return $_SESSION['zhm_uid'];
+        return $_SESSION['rpuid'];
     }
 
 }
