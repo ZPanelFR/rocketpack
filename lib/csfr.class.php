ab99f5bad13aa6599939c06ae8a173c7df52efa1
Bobby Allen <bobbyallen.uk@gmail.com>
2012-07-18 15:46:10 +0100

    Added a new class to handle and enable protection when used against CSFR/XSFR but generating and storing session variables for HTML forms.

---
+++ lib/csfr.class.php
@@ -0,0 +1,68 @@
+<?php
+
+class csfr {
+
+    /**
+     * @param boolean $autokey Automatically generate a CSFR key upon class load.
+     */
+    public function __construct($autokey = false) {
+        if ($autokey == true)
+            $this->GenerateCSFRKey();
+    }
+
+    /**
+     * Geneates a new CSFR key and stores it in a session to be validated later if required.
+     * @return void 
+     */
+    public function GenerateCSFRKey() {
+        return $this->CSFRKeyStore()->Write('csfr_key', md5(microtime()));
+    }
+
+    /**
+     * Retrieve the session generated CSRF key to valiadate and protect against CSFR attacks. 
+     * @return string The current stored CSFR key that has been generated. 
+     */
+    public function CurrentCSFRKey() {
+        return $this->CSFRKeyStore()->Read('csfr_key');
+    }
+
+    /**
+     * Validates the submittetd form's CSRF key to ensure it is corect.
+     * @param string $submitted_key The submitted FORM CSFR key.
+     * @return boolean 
+     */
+    public function ValidateCSFRKey($submitted_key) {
+        if ($this->CSFRKeyStore()->Read('csfr_key') == $submitted_key)
+            return true;
+        return false;
+    }
+
+    /**
+     * Handles the management and storage of CSFR keys in a session.
+     * @return object An instance of the session library of which we use to store and retrieve the keys from. 
+     */
+    private function CSFRKeyStore() {
+        return new session();
+    }
+
+    /**
+     * Builds and returns a hidden HTML FORM tag of which can be used in forms to output the CSFR tag.
+     * @return string The HTML hidden input tag complete with the correct CSFR tag. 
+     */
+    public function BuildCSFRFormTag() {
+        return "<input id=\"csfr_key\" name=\"csfr_key\" type=\"hidden\" value=\"" . $this->CurrentCSFRKey() . "\" />";
+    }
+
+    /**
+     * Validates a POST request which validates the 'csfr_key' hidden field automatically and returns if the key was successful or not!
+     * @return boolean 
+     */
+    public function ValidateCSFRFormTag() {
+        if (isset($_POST['csfr_key']) && ($_POST['csfr_key'] == $this->CurrentCSFRKey()))
+            return true;
+        return false;
+    }
+
+}
+
+?>
